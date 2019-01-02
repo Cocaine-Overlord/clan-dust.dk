65c16092c7bb8cf356e97918a183de74d76fc6a6
root <root@raspberrypi>
2019-01-02 23:00:41 +0100

    test of changes

--- links.php
+++ links.php
@@ -6,7 +6,7 @@
 	<p><a href="http://192.168.0.193:8081/wordpress">Everythingqute.com</a> - Everythingqute webshop</p>
 	<p><a href="http://192.168.0.193:8080">dust-test.dk</a> - Original Dust Wordpress site</p>
 	<p><a href="http://mvc5.clandust.com" target="_blank">clan-dust.dk</a> - MVC5 site</p>
-	<p><a href="http://flyfish-jandelaporte.com" target="_blank">flyfish</a> - Jan's site</p>
+	<p><a href="http://flyfish-jandelaporte.com" target="_blank">flyfish</a> - Jan's site suck my balls</p>
 </div>
 <?php
 	include("common/footer.php");
